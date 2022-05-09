d74e1e4a95d35d8bea0a68cd22e77261c88074dd
KJ Johnson <kjohns95@emich.edu>
2022-05-09 16:51:38 -0400

    Added find submissionByName function. Need to find out how to test properly.

--- script.js
+++ script.js
@@ -40,4 +40,47 @@ function editSubmission(array, index, score) {
   array[index].score = score;
   array[index].passed = passed >= 60 ? true : false;
 }
-console.log(submissions);
+
+// editSubmission(submissions, 2, 65);
+// console.log(submissions);
+
+// Declare a function named findSubmissionByName
+// Parameter(s): array, name
+// Functionality: return the object in the array that has the provided name. Use the find method.
+
+function findSubmissionByName(array, name) {
+  const findStudent = array.find((item) => {
+    return item.name === name;
+  });
+
+  if (findStudent) {
+    return findStudent;
+  } else {
+    return "No student with that name is found.";
+  }
+}
+console.log(findStudent("Jill"));
+
+// Declare a function named findLowestScore
+// Parameter(s): array
+// Functionality: return the object in the array that has the lowest score. Use the forEach method to loop through the whole array.
+
+function findLowestScore(array) {}
+
+// Declare a function named findAverageScore
+// Parameter(s): array
+// Functionality: return the average quiz score.  Use a for...of loop.
+
+function findAverageScore(array) {}
+
+// Declare a function named filterPassing
+// Parameter(s): array
+// Functionality: return a new array using the filter method. The filter method should find objects in the array that have passing scores.
+
+function filterPassing(array) {}
+
+// Declare a function named filter90AndAbove
+// Parameter(s): array
+// Functionality: return a new array using the filter method. The filter method should find objects in the array that have scores greater than or equal to 90.
+
+function filter90AndAbove(array) {}
